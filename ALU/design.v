41fe6220d88f82e878db89158186215e692b78f5
Karthika K <119489367+karthikajayasakthi@users.noreply.github.com>
2024-12-25 11:07:18 +0530

    Update and rename ALU_pro.md to design.v

---
+++ ALU/design.v
@@ -0,0 +1,16 @@
+//BEHAVIORAL
+
+module alu_4bit(a,b,opcode,result);
+  input [3:0] a,b;
+  input [1:0] opcode;
+  output reg [3:0] result;
+  always@(*)
+begin
+  case(opcode)
+2'b00:result=a+b;
+2'b01:result=a-b;
+2'b10:result=a*b;
+2'b11:result=a/b;
+  endcase
+end
+endmodule
